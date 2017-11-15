37dbd8826d7c63e75caec194d1519d8d76cf1341
Kyle Altendorf <kyle.altendorf@epcpower.com>
2017-11-15 08:04:48 -0500

    confish/fource and git aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,23 @@
 function ct
     cd /epc/t/$argv
 end
+
+function confish
+    eval $EDITOR ~/.config/fish/config.fish
+end
+alias fource 'source ~/.config/fish/config.fish'
+
+alias gs 'git status'
+alias gc 'git commit'
+alias gd 'git diff'
+alias gdt 'git difftool'
+alias gdtd 'git difftool --dir-diff'
+alias gmt 'git mergetool'
+alias gdc 'git diff --cached'
+alias gau 'git add -u'
+alias grph 'git rev-parse HEAD'
+
+function gaus
+    gau $argv
+    gs
+end
