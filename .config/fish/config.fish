6cba50f0f37520c290c205bdb5669bcc479c70ad
Kyle Altendorf <sda@fstab.net>
2024-11-01 17:36:25 -0400

    just a checkpoint

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,34 @@
 #    cd /epc/t/$argv
 #end
 
+function td
+    sudo tailscale down
+end
+
+function tu
+    sudo tailscale down
+    sudo tailscale up --reset --accept-routes --shields-up
+end
+
+function tun
+    sudo tailscale down
+    sudo tailscale up --reset --accept-routes --shields-up --exit-node 100.96.223.31
+end
+
+function fix-mouse
+    sudo rmmod psmouse
+    sudo modprobe psmouse
+    sleep 5
+    sudo udevadm trigger -s input
+end
+
+function fix-kde
+    killall plasmashell kwin
+    kstart5 kwin
+    sleep 5
+    kstart5 plasmashell
+end
+
 function cr
     cd /epc/repos/$argv
 end
@@ -30,6 +58,7 @@ function byebyedocker
     docker rmi -f (docker images -qa)
     docker volume rm (docker volume ls -q)
     docker network rm (docker network ls -q)
+    docker system prune --all --force
 end
 
 
@@ -60,6 +89,9 @@ alias grph 'git rev-parse HEAD'
 alias gsu 'git submodule update --init'
 alias gsp 'git stash && git pull --rebase && git stash pop'
 alias gfi 'git checkout develop && git flow init --defaults && git checkout -'
+alias gcan 'gc --amend --no-edit'
+
+alias xcs 'xclip -selection clipboard'
 
 alias h heroku
 alias sr 'ssh ubuntu@pi'
@@ -74,6 +106,7 @@ alias mw 'mosh altendky@w550s'
 #alias mwr 'mosh --ssh="ssh -p 2203" --server=mosh-server-upnp chia@home.fstab.net'
 alias ms 'mosh altendky@server'
 alias msr 'mosh --ssh="ssh -p 2204" --server=mosh-server-upnp altendky@home.fstab.net'
+alias brewon 'eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)'
 
 function keybase-hide
     echo '{"method": "list"}' | keybase chat api | jq --raw-output '.result.conversations[] | select(.channel.members_type == "impteamnative" and .unread == false) | .channel.name' | xargs --no-run-if-empty --max-lines=1 --max-procs=10 keybase chat hide
@@ -85,8 +118,8 @@ function gaus
     gs
 end
 
+set --export _INTELLIJ_FORCE_PREPEND_PATH ''
 set --export PIPX_BIN_DIR ~/.local/bin/pipx
-#set --export PATH /epc/bin ~/.local/bin ~/.local/bin_pipx ~/.local/phabricator/arcanist/bin $PATH
 set --export PYTHONDONTWRITEBYTECODE 1
 set --export PYTHON_CONFIGURE_OPTS --enable-shared
 set --export PIP_REQUIRE_VIRTUALENV 1
@@ -95,15 +128,15 @@ set --export EDITOR vim
 
 # https://github.com/pyenv/pyenv/issues/32#issuecomment-482980350
 set --export PYENV_ROOT $HOME/.pyenv
-set --export PATH $PYENV_ROOT/shims $PYENV_ROOT/bin $PATH
 #set -Ux PYENV_ROOT $HOME/.pyenv
 #set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
 
-status is-login; and pyenv init --path | source
-pyenv init - | source
+set --export N_PREFIX $HOME/.n
+
+#status is-login; and pyenv init --path | source
+#pyenv init - | source
 
 #set --export PYENV_ROOT $HOME/.pyenv
-#set --export PATH $PYENV_ROOT/bin $PATH
 #status --is-interactive; and pyenv init - | source
 #status --is-interactive; and pyenv virtualenv-init - | source
 
@@ -118,3 +151,16 @@ if [ -e ~/.config/chips/build.fish ] ; . ~/.config/chips/build.fish ; end
 
 # Added by Krypton
 #set -x GPG_TTY (tty)
+
+if [ "$ALTENDKY_FISH_CONFIGURED" = "1" ]
+    exit 0
+end
+set --export ALTENDKY_FISH_CONFIGURED "1"
+
+set --export PATH $PYENV_ROOT/shims $PYENV_ROOT/bin $PATH
+#set --export PATH /epc/bin ~/.local/bin ~/.local/bin_pipx ~/.local/phabricator/arcanist/bin $PATH
+set --export PATH ~/.local/bin ~/.local/bin/pipx $PATH
+set --export PATH ~/.cargo/bin $PATH
+set --export PATH $N_PREFIX/bin $PATH
+#set --export PATH $PYENV_ROOT/bin $PATH
+set --export PATH $PATH /home/altendky/.local/bin/pipx
